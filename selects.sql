eefec2439e2d0f5bbac533acfc32dd480e8a3766
Lucas Alves <varela.alves@outlook.com>
2021-09-16 23:42:07 -0300

    inserts ok

--- selects.sql
+++ selects.sql
@@ -1,10 +1,11 @@
 /* SELECTS */
 
-/* Lista de consultas feitas pelos veterinários OK*/
+/* Lista de consultas feitas pelos veterinários depois de 2019 OK*/
 SELECT V.crmv, V.regiao, C.data_consulta, C.motivo, C.diagnostico
     FROM  Consulta as C
     JOIN  Veterinario as V  
-    ON C.codigo_Veterinario = V.codigo_Usuario;
+    ON C.codigo_Veterinario = V.codigo_Usuario
+    HAVING C.data_consulta BETWEEN '2019-01-01' AND '2021-09-16';
 
 /*Lista de vacinas que os pets tomaram OK*/
 SELECT P.nome, A.codigo_Vacina, A.data_ap, A.data_prox
@@ -16,15 +17,16 @@ SELECT P.nome, A.codigo_Vacina, A.data_ap, A.data_prox
 SELECT U.nome, P.nome 
     FROM Dono_de_Pet as D,
         Pet as P,
-        USUARIO as U
+        Usuario as U
     WHERE D.codigo_Usuario = P.codigo_Dono_de_Pet AND U.CPF = D.codigo_Usuario;
 
 
-
-/*Lista de pets que um veterinário realizou uma consulta e as vacinas do pet*/
-SELECT 
-    FROM
-    
+/*Lista de pets que os veterinários realizaram uma consulta*/
+SELECT DISTINCT U.nome, P.nome 
+    FROM Pet as P
+    JOIN (Veterinario as V,
+          Usuario as U, Consulta C)
+    ON V.codigo_Usuario = U.CPF AND V.codigo_Usuario = C.codigo_Veterinario AND C.codigo_Pet = P.codigo_identificador;
 
 /*Lista de pets que nasceram depois de 2010 OK*/
 SELECT P.nome, P.idade, P.raca
